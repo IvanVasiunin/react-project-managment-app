b0315e9145e1175d70f37ec82b169fb695c4d6d0
Ivan <ivanvasyunin@gmail.com>
2023-11-03 14:26:22 +0100

    NewProject and Input components added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import ProjectSidebar from './ProjectsSidebar';
+import NewProject from './NewProject';
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectSidebar />
+      <NewProject />
     </main>
   );
 }
